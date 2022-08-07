86fd7d816cbe65ae7c95e17243df78a54a5bdc64
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-07 19:18:23 +0300

    completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color')
+const bodyEl = document.querySelector('body')
+const spanEl = document.querySelector('.color')
+
+const changeColor = event => {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = spanEl.textContent;
+}
+
+buttonEl.addEventListener('click', changeColor)
+
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,46 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const boxControlEl = document.querySelector('#controls');
+const inputEl = boxControlEl.firstElementChild
+const createButtonEl = document.querySelector('[data-create]')
+const destroyButtonEl = document.querySelector('[data-destroy]')
+const divBoxes = document.querySelector('#boxes')
+
+
+createButtonEl.addEventListener('click', getAmount)
+
+destroyButtonEl.addEventListener('click', destroyBoxes) 
+
+function getAmount() {
+  var amount = inputEl.value;
+  createBoxes(amount);
+}
+
+function destroyBoxes() {
+  divBoxes.innerHTML = ' ';
+}
+  
+
+function createBoxes(amount) {
+  const basicSize = 30;
+  for (let i = 0; i < amount; i++){
+    const size = basicSize + i * 10;
+    const divEl = document.createElement('div')
+
+  divEl.style.background = getRandomHexColor();
+  divEl.style.width = size + "px"
+    divEl.style.height = size + "px"
+  divBoxes.append(divEl)
+    
+  }
+}
+
+
+
+
+
+
